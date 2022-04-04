a4a24111e390821e4c9f74a925d9533d71bb978a
OmarCahuanaRios <omaroceanuz@gmail.com>
2022-04-03 20:22:36 -0500

    second commit

--- src/app.py
+++ src/app.py
@@ -1,6 +1,10 @@
 
+
+
+
 from flask import Flask,render_template, request,redirect,url_for,flash
 from flask_login import LoginManager,login_user,logout_user,login_required
+from urllib3 import Retry
 from config import config
 from flask_mysqldb import MySQL
 from models.ModelUser import ModelUser
@@ -24,8 +28,6 @@ def index():
     return redirect(url_for('login'))
 @app.route('/login',methods=['GET','POST'])
 def login():
-  
-    
     if request.method=='POST':
         user=User(0,request.form['username'],request.form['password'])
         logged_user=ModelUser.login(db,user)
@@ -44,14 +46,72 @@ def login():
         ("no recibo")
         return render_template('auth/login.html')
 
+
+
+    
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
         
-@app.route('/home')       
+@app.route('/home',methods=['GET','POST'])       
 def home():
+    if request.method=='POST':
+    
+        selectedValue=request.form['Zona']
+        return redirect(url_for('GeneralMap',selectedValue=selectedValue))
+
     return render_template('home.html')     
+
+
+@app.route('/<selectedValue>',methods=['GET','POST'])
+def GeneralMap(selectedValue):
+    if request.method=='POST':
+        return redirect(url_for('Simulator'))
+    if selectedValue=='GeneralMap':
+        return render_template('GeneralMap.html')
+    if selectedValue=='SpecificMap':
+        return render_template('SpecificMap.html')
+
+
+@app.route('/estadisticas',methods=['GET','POST'])
+def estadisticas():
+        
+    return render_template('Result.html')
+        
+
+@app.route('/simulator',methods=['GET','POST'])
+def Simulator():
+    if request.method=='POST':
+       return redirect(url_for('resultados'))
+    return render_template('simulator.html')
+
+
+@app.route('/result',methods=['GET','POST'])
+def resultados():
+    if request.method=='POST':
+        return redirect(url_for('summary'))
+    return render_template('Result.html')
+
+
+
+@app.route('/summary',methods=['GET','POST'])
+def summary():
+    return render_template("Summary.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route('/protected')
